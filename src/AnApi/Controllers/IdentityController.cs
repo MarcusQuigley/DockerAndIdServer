b2f9e4c25166c30cd1014eb3e6c3ad05b3902100
Marcus Quigley <marcusquigley@gmail.com>
2022-01-03 13:43:24 -0500

    adding id and cleint

---
+++ src/AnApi/Controllers/IdentityController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AnApi.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("identity")]
+    public class IdentityController : ControllerBase
+    {
+
+        private readonly ILogger<IdentityController> _logger;
+
+        public IdentityController(ILogger<IdentityController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
